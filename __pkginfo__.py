c712f2c9616c71a96c2e8d7cf200da9822d835f5
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-07-12 12:54:48 +0100

    Bump version v0.0.0 -> v0.1.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -16,5 +16,5 @@ __all__ = [
 		"extras_require",
 		]
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 extras_require = {}
--- simple503/__init__.py
+++ simple503/__init__.py
@@ -61,7 +61,7 @@ else:
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.0.0"
+__version__: str = "0.1.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = [
