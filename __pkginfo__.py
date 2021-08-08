1bd8626e6d11353bd0bcd558f2e7ad84bfca5164
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-08-08 22:58:46 +0100

    Correct dependency name

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -13,4 +13,7 @@
 
 __all__ = ["extras_require"]
 
-extras_require = {}
+extras_require = {
+		"incremental": ["beautifulsoup4>=4.9.3", "html5lib>=1.1"],
+		"all": ["beautifulsoup4>=4.9.3", "html5lib>=1.1"]
+		}
